5f76c71ba7f19a8bd1e928b3ca20ae8aa8673fc5
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-05 22:45:49 +0400

    Ex_10

---
+++ Ex_10/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Ведите первое число");
+string? A = ReadLine();
+double a;
+if (double.TryParse(A, out a))
+    {
+      // Преобразовать A в а  
+    }
+else
+    {
+        Console.WriteLine("Ошибка ввода");
+    }
+Console.WriteLine("Ведите второе число");
+string? B = ReadLine();
+double b;
+if (double.TryParse(B, out b))
+    {
+       // Преобразовать B в b  
+    }
+else
+    {
+        Console.WriteLine("Ошибка ввода");
+    }
+if (a == b*b) 
+    {
+    Console.WriteLine("Первое число является квадратом второго числа");  
+    }
+else 
+    {
+    Console.WriteLine("Первое число не является квадратом второго числа");  
+    }
+
