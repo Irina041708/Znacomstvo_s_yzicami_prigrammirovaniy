4e57ab3570d00fa78635281640ec4c41e1921402
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-08 20:00:40 +0400

    Ex_15_zad_4_chetnie_chisla

---
+++ Ex_15_Zad_4/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N. */
+Console.WriteLine("Ведите число");
+string N = Console.ReadLine()??"";
+int n;
+int i = 0;
+
+
+if ((int.TryParse(N, out n)))
+
+    {   while ( i <= n) 
+            {
+                Console.WriteLine( i ); 
+                         
+             i += 2; 
+            }
+          
+    }
