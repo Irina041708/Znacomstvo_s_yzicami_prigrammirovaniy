970a06058165ce0190321f7ff2da714ef68c2777
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-12 15:16:18 +0400

    Ex_0010

--- L№2/Ex_0010/Program.cs
+++ L№2/Ex_0010/Program.cs
@@ -1,5 +1,10 @@
-﻿int Max (int arg1, int arg2, int arg3);
-
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
 
 
 int a1 = 15;
@@ -14,17 +19,11 @@ int a3 = 75;
 int b3 = 85;
 int c3 = 3;
 
-int max = a1;
+/*int max1 = Max (a1,b1,c1);
+int max2 = Max (a2,b2,c2);
+int max3 = Max (a3,b3,c3);
+int max = Max (max1,max2,max3);*/
 
-if (max < b1) max = b1;
-if (max < c1) max = c1;
-
-if (max < a2) max = a2;
-if (max < b2) max = b2;
-if (max < c2) max = c2;
-
-if (max < a3) max = a3;
-if (max < b3) max = b3;
-if (max < c3) max = c3;
+int max = Max (Max (a1,b1,c1),Max (a2,b2,c2),Max (a3,b3,c3));
 
 Console.WriteLine (max);
