4bd1096d7aa627f8006442da71836fb892d406d6
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-12 13:18:11 +0400

    Zad№2

--- DZ№2/Zad№2/Program.cs
+++ DZ№2/Zad№2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.*/
+Console.Clear();
+int x = new Random().Next(100,1000);
+string num = f(x);
+Console.WriteLine ($" {x}");
+Console.WriteLine ($" {num} - третья цифра заданного числа");
+string f ( int x )
+{
+   
+           int a2 = x%10;
+        string b2 = a2.ToString();
+        string num = b2;
+        return num;
+}
+
